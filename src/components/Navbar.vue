070e9081c9b0ab6b8f0cca0e034731889573ffc4
Victor_Dev <165293570+victorhenriquemariano@users.noreply.github.com>
2024-06-13 21:40:31 -0300

    teste

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@
         <router-link to="/">
             <img src="" alt="" id="logo">
         </router-link>
-        <router-link to="/">Home</router-link> |
+        <router-link to="/">Homee</router-link> |
         <router-link to="/pedidos">Pedidos</router-link>
     </div>
 </template>
