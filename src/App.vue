9fa2b235e4cd5431a29ce9b9e1abd761bc41a089
Victor_Dev <165293570+victorhenriquemariano@users.noreply.github.com>
2024-06-13 22:08:01 -0300

    Aula22

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <navbar />
+  <navbar :logo="logo_src" :alt="app_name"/>
   <router-view />
   <Footer />
 </template>
@@ -12,10 +12,35 @@ export default {
   components: {
     Navbar,
     Footer
+  },
+  data() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "/img/Make Your Burger"
+    }
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap');
 
+  *{
+    font-family: "Inter", sans-serif;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  .main-container {
+    margin: 50px;
+    min-height: 200px;
+  }
+
+  h1{
+    text-align: center;
+    font-size: 40px;
+    margin-bottom: 30px;
+    color: #222;
+  }
 </style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-    <p>Footer</p>
+    <footer id="footer">
+        <p>Make Your Burger &copy; 2024</p>
+    </footer>
 </template>
 
 <script>
@@ -7,3 +9,16 @@ export default {
     name: 'Footer'
 }
 </script>
+
+<style scoped>
+    #footer {
+        height: 200px;
+        background-color: #222;
+        border-top: 4px solid #111;
+        color: #FCBA03;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+    }
+</style>
